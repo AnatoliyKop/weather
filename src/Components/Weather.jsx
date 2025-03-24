37522aeae856707aadac5155e6eb8e296c9b8b47
AnatoliyKop <ttolea90tolea132@gmail.com>
2025-03-24 10:46:55 +0100

    First commit

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Weather = ({weather,message}) => {
+    if(message){
+        return (<div className={'infoWeath'}>{message}</div>)
+    }
+
+    return (
+        <div className={'infoWeath'} >
+            <p>Location: {weather.country}, {weather.city}</p>
+            <p>Temp: {weather.temp}</p>
+            <p>Pressure:{weather.pressure}</p>
+            <p>Sunset: {new Date(weather.sunset*1000).toLocaleTimeString()}</p>
+        </div>
+    );
+};
+
+export default Weather;
